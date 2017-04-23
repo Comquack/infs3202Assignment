e454f99e7f6f54d4118275ac6731b5a916e65d80
Comquack <s4229599@labs.eait.uq.edu.au>
2017-04-24 08:53:01 +1000

    connect to sql add server details

--- login.php
+++ login.php
@@ -1,5 +1,16 @@
 <?php
 	session_start();
+	$servername = "localhost";
+	$username = "username";
+	$password = "password";
+// Create connection
+	$conn = new mysqli($servername, $username, $password);
+
+	// Check connection
+	if ($conn->connect_error) {
+		//die("Connection failed: " . $conn->connect_error);
+	} 
+	echo "Connected successfully";
 	if($_REQUEST['username']=="infs" && $_REQUEST['password']=="3202"){
 		$_SESSION['username'] = "infs";
 		$_SESSION['password'] = "3202";
