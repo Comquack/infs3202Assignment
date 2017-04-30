7b5c31eafe6f78b5dd67dc7306c30c45b528ba74
Ben-PC\BenJamin <benwildie@hotmail.com>
2017-04-30 13:17:23 +1000

    Map now gives directions to airports from home

--- map.js
+++ map.js
@@ -1,5 +1,6 @@
 var API = "AIzaSyCMy7POvCCYeCbQz5TO49LqidMkCEFin_A" 
  var map;
+ var pos;
   var Label = {
         Home: {
           label: 'H'
@@ -8,10 +9,6 @@ var API = "AIzaSyCMy7POvCCYeCbQz5TO49LqidMkCEFin_A"
           label: 'A'
         }
       };
- //var geocoder = new google.maps.Geocoder();
-
-
-
       function initMap() {
 				var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 8,
@@ -23,7 +20,6 @@ $.ajax({
   type: 'GET',
   url: '../mapcall.php',
   success: function (data) {
-	  console.log(data);
 	   $(data).find("airports").each(function(){
 		var address = $(this).attr("address");
 		geocoder.geocode({'address': address}, function(results, status) {
@@ -32,50 +28,39 @@ $.ajax({
               map: map,
               position: results[0].geometry.location
 			})
+			marker.addListener('click', function() {
+			var directionsService = new google.maps.DirectionsService();
+			directionsDisplay = new google.maps.DirectionsRenderer();
+			 var request = {
+				origin: pos,
+				destination: results[0].geometry.location,
+				travelMode: 'DRIVING'
+			};
+			directionsDisplay.setMap(map);
+			directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      directionsDisplay.setDirections(result);
+    }
+  });
+
+
+        });
 			}
 		})
 
    });
-   // document.getElementsByTagName("airport").innerHTML = data;
-	// console.log(document.getElementsByTagName("airport").innerHTML);
-	 		
   }
 });
 
-        /*request.onreadystatechange = function() {
-			var xml = request.responseXML;
-            var airports = xml.documentElement.getElementsByTagName('airports');
-            Array.prototype.forEach.call(airports, function(markerElem) {
-              var name = markerElem.getAttribute('name');
-              var address = markerElem.getAttribute('address');
-              var infowincontent = document.createElement('div');
-              var strong = document.createElement('strong');
-              strong.textContent = name
-              infowincontent.appendChild(strong);
-              infowincontent.appendChild(document.createElement('br'));
-              var text = document.createElement('text');
-              text.textContent = address
-              infowincontent.appendChild(text);
-              var icon = customLabel[type] || {};
-              var marker = new google.maps.Marker({
-                map: map,
-                position: point,
-                label: icon.label
-              });
-            });
-
-		}*/
 
 
 if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
+            pos = {
               lat: position.coords.latitude,
 			  lng: position.coords.longitude
 			}
-			console.log(pos);
 			var centre = new google.maps.LatLng(pos);
-			console.log(centre);
 			map.panTo(centre);
 			var icon = Label["Home"] || {};
 			var marker = new google.maps.Marker({
